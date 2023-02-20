97abf80105a2415f8740adc3603d4df5c61baa59
AnurajDairyProducts <125817104+AnurajDairyProducts@users.noreply.github.com>
2023-02-21 01:45:52 +0530

    entire project upload

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   $("#user").on("click",function(){
+       alert(" Hare Krsna, Welcome to AnuRaj Dairy Products!!!!");
+   });
+
+   $("#cart").on("click",function(){
+       alert("Cart");
+   })
+
+    $("#menu").on("click",function(){
+       $("#icons").toggle();
+    });
+});
